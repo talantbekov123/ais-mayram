386b5f281602b2bb54c90b01a74736f8e1f0e82b
Kairat Talantbekov <talantbekov123@gmail.com>
2020-10-06 16:36:15 +0600

    Add show list function

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 const fs = require('fs');
 const multer = require('multer');
+var path = require('path');
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
     cb(null, 'public/')
@@ -10,15 +11,23 @@ const storage = multer.diskStorage({
     cb(null, file.originalname)
   }
 })
-const upload = multer({storage: storage})
+const upload = multer({ storage: storage })
 
 /* GET home page. */
-router.get('/uploads', function(req, res, next) {
-  let { link = null }  = req.query;
-  res.render('index', { link });
+router.get('/uploads', function (req, res, next) {
+  const directoryPath = path.join(__dirname, '../public');
+  fs.readdir(directoryPath, function (err, files) {
+    //handling error
+    if (err) {
+      console.log('Unable to scan directory: ' + err);
+    }
+
+    let { link = null } = req.query;
+    res.render('index', { link, files });
+  });
 });
 
-router.post('/', upload.any(), function(req, res, next) {
+router.post('/', upload.any(), function (req, res, next) {
   res.redirect('/uploads?link=' + req.files[0].originalname)
 });
 
